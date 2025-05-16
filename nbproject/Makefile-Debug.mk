244bc26572e2021b462a8e39f790b18896843e67
Weisni <marcel.mainz@googlemail.com>
2025-05-16 17:29:02 +0200

    added nbproject files for build action

---
+++ nbproject/Makefile-Debug.mk
@@ -0,0 +1,195 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
+include Makefile
+# Include makefile containing local settings
+ifeq "$(wildcard nbproject/Makefile-local-Debug.mk)" "nbproject/Makefile-local-Debug.mk"
+include nbproject/Makefile-local-Debug.mk
+endif
+endif
+
+# Environment
+MKDIR=gnumkdir -p
+RM=rm -f 
+MV=mv 
+CP=cp 
+
+# Macros
+CND_CONF=Debug
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+OUTPUT_SUFFIX=elf
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/R1-1-P1_1-C1-1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+else
+IMAGE_TYPE=production
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/R1-1-P1_1-C1-1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+endif
+
+ifeq ($(COMPARE_BUILD), true)
+COMPARISON_BUILD=-mafrlcsj
+else
+COMPARISON_BUILD=
+endif
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Source Files Quoted if spaced
+SOURCEFILES_QUOTED_IF_SPACED=LibConfig.c main.c
+
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/LibConfig.o ${OBJECTDIR}/main.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/LibConfig.o.d ${OBJECTDIR}/main.o.d
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/LibConfig.o ${OBJECTDIR}/main.o
+
+# Source Files
+SOURCEFILES=LibConfig.c main.c
+
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+
+# The following macros may be used in the pre and post step lines
+_/_=\\
+ShExtension=.bat
+Device=PIC24FJ1024GB610
+ProjectDir="C:\Users\marce\Documents\GitHub\R1-1-P1_1-C1-1"
+ProjectName=R1-1-P1_1-C1-1
+ConfName=Debug
+ImagePath="dist\Debug\${IMAGE_TYPE}\R1-1-P1_1-C1-1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}"
+ImageDir="dist\Debug\${IMAGE_TYPE}"
+ImageName="R1-1-P1_1-C1-1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}"
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IsDebug="true"
+else
+IsDebug="false"
+endif
+MDFUHostPath="C:\Program Files\Microchip\MPLABX\v6.25\mplab_platform\mplab-pymdfu\bin\windows\pymdfu-bin.exe"
+PYFWImageBuilderPath="C:\Program Files\Microchip\MPLABX\v6.25\mplab_platform\mplab-pymdfu\bin\windows\pyfwimagebuilder-bin.exe"
+
+.build-conf:  .pre ${BUILD_SUBPROJECTS}
+ifneq ($(INFORMATION_MESSAGE), )
+	@echo $(INFORMATION_MESSAGE)
+endif
+	${MAKE}  -f nbproject/Makefile-Debug.mk ${DISTDIR}/R1-1-P1_1-C1-1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+	@echo "--------------------------------------"
+	@echo "User defined post-build step: [C:\Program Files\AutoHotkey\AutoHotkey.exe ${ProjectDir}\GitVersion\CopyHexfile.ahk ${ImagePath} ${ProjectDir}]"
+	@C:\Program Files\AutoHotkey\AutoHotkey.exe ${ProjectDir}\GitVersion\CopyHexfile.ahk ${ImagePath} ${ProjectDir}
+	@echo "--------------------------------------"
+
+MP_PROCESSOR_OPTION=24FJ1024GB610
+MP_LINKER_FILE_OPTION=,--script="KG-Bootloader\Bootloader.X\p24FJ1024GB610_App.gld"
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/LibConfig.o: LibConfig.c  .generated_files/flags/Debug/2bd53c45d817a35ab447b5e59c86f0f1db1c24dc .generated_files/flags/Debug/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/LibConfig.o.d 
+	@${RM} ${OBJECTDIR}/LibConfig.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  LibConfig.c  -o ${OBJECTDIR}/LibConfig.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/LibConfig.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_Debug=$(CND_CONF)    $(COMPARISON_BUILD)  --partition 1 -mlarge-code -O0 -D__PIC24FJ1024GB610__ -DDEBUG -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/Debug/e47ef880943f902a5e0dac9be4a7fb8e15e74c3a .generated_files/flags/Debug/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/main.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_Debug=$(CND_CONF)    $(COMPARISON_BUILD)  --partition 1 -mlarge-code -O0 -D__PIC24FJ1024GB610__ -DDEBUG -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+else
+${OBJECTDIR}/LibConfig.o: LibConfig.c  .generated_files/flags/Debug/56c3c925a99e907f7209fd9590fd18675f876a04 .generated_files/flags/Debug/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/LibConfig.o.d 
+	@${RM} ${OBJECTDIR}/LibConfig.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  LibConfig.c  -o ${OBJECTDIR}/LibConfig.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/LibConfig.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_Debug=$(CND_CONF)    $(COMPARISON_BUILD)  --partition 1 -mlarge-code -O0 -D__PIC24FJ1024GB610__ -DDEBUG -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/Debug/91a11cf30477bc3accd347519416ae84b90bf1d6 .generated_files/flags/Debug/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/main.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_Debug=$(CND_CONF)    $(COMPARISON_BUILD)  --partition 1 -mlarge-code -O0 -D__PIC24FJ1024GB610__ -DDEBUG -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemblePreproc
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${DISTDIR}/R1-1-P1_1-C1-1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  KG-Lib/dist/PIC24FJ1024GB610_Debug/debug/KG-Lib.a  KG-Bootloader/Bootloader.X/p24FJ1024GB610_App.gld
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o ${DISTDIR}/R1-1-P1_1-C1-1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}    KG-Lib\dist\PIC24FJ1024GB610_Debug\debug\KG-Lib.a  -mcpu=$(MP_PROCESSOR_OPTION)        -D__DEBUG=__DEBUG   -omf=elf -DXPRJ_Debug=$(CND_CONF)    $(COMPARISON_BUILD)   -mreserve=data@0x800:0x81B -mreserve=data@0x81C:0x81D -mreserve=data@0x81E:0x81F -mreserve=data@0x820:0x821 -mreserve=data@0x822:0x823 -mreserve=data@0x824:0x827 -mreserve=data@0x82A:0x84F   -Wl,--local-stack,--partition,1,,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D__DEBUG=__DEBUG,,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--no-isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,${DISTDIR}/memoryfile.xml$(MP_EXTRA_LD_POST)  -mdfp="${DFP_DIR}/xc16" 
+	
+else
+${DISTDIR}/R1-1-P1_1-C1-1.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  KG-Lib/dist/PIC24FJ1024GB610_Debug/production/KG-Lib.a KG-Bootloader/Bootloader.X/p24FJ1024GB610_App.gld KG-Bootloader/Bootloader.X/dist/DebugConfig_PIC24FJ1024GB610/production/Bootloader.X.production.hex
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o ${DISTDIR}/R1-1-P1_1-C1-1.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}    KG-Lib\dist\PIC24FJ1024GB610_Debug\production\KG-Lib.a  -mcpu=$(MP_PROCESSOR_OPTION)        -omf=elf -DXPRJ_Debug=$(CND_CONF)    $(COMPARISON_BUILD)  -Wl,--local-stack,--partition,1,,--defsym=__MPLAB_BUILD=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--no-isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,${DISTDIR}/memoryfile.xml$(MP_EXTRA_LD_POST)  -mdfp="${DFP_DIR}/xc16" 
+	${MP_CC_DIR}\\xc16-bin2hex ${DISTDIR}/R1-1-P1_1-C1-1.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} -a  -omf=elf --offset 0x0  -mdfp="${DFP_DIR}/xc16" 
+	
+	@echo "Creating unified hex file"
+	@"C:/Program Files/Microchip/MPLABX/v6.25/mplab_platform/platform/../mplab_ide/modules/../../bin/hexmate" --edf="C:/Program Files/Microchip/MPLABX/v6.25/mplab_platform/platform/../mplab_ide/modules/../../dat/en_msgs.txt" -break=1003000  ${DISTDIR}/R1-1-P1_1-C1-1.${IMAGE_TYPE}.hex KG-Bootloader/Bootloader.X/dist/DebugConfig_PIC24FJ1024GB610/production/Bootloader.X.production.hex -odist/${CND_CONF}/production/R1-1-P1_1-C1-1.production.unified.hex
+
+endif
+
+.pre:
+	@echo "--------------------------------------"
+	@echo "User defined pre-build step: [C:\Program Files\AutoHotkey\AutoHotkey.exe ${ProjectDir}\GitVersion\GitVersion.ahk ${ProjectDir}]"
+	@C:\Program Files\AutoHotkey\AutoHotkey.exe ${ProjectDir}\GitVersion\GitVersion.ahk ${ProjectDir}
+	@echo "--------------------------------------"
+
+# Subprojects
+.build-subprojects:
+	cd KG-Lib && ${MAKE}  -f Makefile CONF=PIC24FJ1024GB610_Debug
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+	cd KG-Bootloader/Bootloader.X && ${MAKE}  -f Makefile CONF=DebugConfig_PIC24FJ1024GB610 TYPE_IMAGE=DEBUG_RUN
+else
+	cd KG-Bootloader/Bootloader.X && ${MAKE}  -f Makefile CONF=DebugConfig_PIC24FJ1024GB610
+endif
+
+
+# Subprojects
+.clean-subprojects:
+	cd KG-Lib && rm -rf "build/PIC24FJ1024GB610_Debug" "dist/PIC24FJ1024GB610_Debug"
+	cd KG-Bootloader/Bootloader.X && rm -rf "build/DebugConfig_PIC24FJ1024GB610" "dist/DebugConfig_PIC24FJ1024GB610"
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${OBJECTDIR}
+	${RM} -r ${DISTDIR}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,14 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# Release configuration
+CND_ARTIFACT_DIR_Release=dist/Release/production
+CND_ARTIFACT_NAME_Release=R1-1-P1_1-C1-1.production.hex
+CND_ARTIFACT_PATH_Release=dist/Release/production/R1-1-P1_1-C1-1.production.hex
+# Debug configuration
+CND_ARTIFACT_DIR_Debug=dist/Debug/production
+CND_ARTIFACT_NAME_Debug=R1-1-P1_1-C1-1.production.hex
+CND_ARTIFACT_PATH_Debug=dist/Debug/production/R1-1-P1_1-C1-1.production.hex
